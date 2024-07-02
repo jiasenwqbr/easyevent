9666c105a0478064de268593cc0e8684c1d97d26
jiasenwqbr <jasonjiasen@gmail.com>
2024-07-02 09:57:33 +0800

    init project

---
+++ src/test/java/com/easyevent/easyevent/EasyeventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyevent.easyevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyeventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
